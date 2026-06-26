4269f14c8017e6f39876623092a5d4e78364d366
nomocat <luodan_0464@qq.com>
2026-06-26 19:00:10 +0800

    chore: ignore local worktrees in lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,10 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 
-const eslintConfig = [...nextVitals];
+const eslintConfig = [
+  ...nextVitals,
+  {
+    ignores: [".next/**", ".worktrees/**", "node_modules/**"],
+  },
+];
 
 export default eslintConfig;
